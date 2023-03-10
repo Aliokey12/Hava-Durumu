d903f3b45eed274b216bb1ed3fbc9cdc92254ebb
Aliokey12 <126576690+Aliokey12@users.noreply.github.com>
2023-03-10 20:44:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -34,25 +34,5 @@ ${Math.round(result.main.temp_max)}`
 const searchBar = document.getElementById('SearchBar')
 SearchBar.addEventListener('keypress', settQuerry)
 
-function getTime(){
-   var now = new Date();
-   var hour= now.getHours();
-   var minute = now.getMinutes();
-var second = now.getSeconds()
-   var day = now.getDate()
-   var month = now.getMonth()+1;
-   var year = now.getFullYear()
-   
-  
-document.getElementById('hour').innerText=hour;
-document.getElementById('minute').innerText=minute;
-document.getElementById('second').innerText=second;
 
-document.getElementById("date").innerText=day+"/"+month+"/"+year;
-
-
-}
-
-
-setInterval(function(){getTime()},1000)
 
